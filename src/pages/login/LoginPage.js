1d660158194107d394af92832a28297d6b8f27d4
Rodrigo Pereira <rodrigopereira.development@gmail.com>
2026-03-11 22:40:27 -0300

    update

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -33,7 +33,45 @@ class LoginPage extends HTMLElement {
                 </ion-card>
             </div>
         `;
+
+            //referencias 
+        const userInput = this.querySelector('#usuario');
+        const passInput = this.querySelector('#senha');
+        const btnLogin = this.querySelector('#login');
+
+        btnLogin.addEventListener('click', async ()=> {
+            const usuario = userInput.value;
+            const senha = passInput.value;
+            
+            const loading = document.createElement('ion-loading');
+            loading.message = 'Autenticando...';
+            loading.duration = 3000;//aguardar o tempo do loading
+
+            document.body.appendChild(loading);
+            await loading.present();
+            await loading.onDidDismiss();
+
+            if (usuario == 'admin' && senha == 'admin') {
+                toast('Login realizado com sucesso', 'success');
+                document.querySelector('ion-router').push('/home', 'forward');
+            } else {
+                toast('Usuário ou senha incorretos');
+            }
+        })
+
+        async function toast(mensagem, color = 'danger') {
+            const toast = document.createElement('ion-toast');
+            toast.message = mensagem;
+            toast.color = color;
+            toast.duration = 2000;
+            toast.position = 'bottom';
+
+            document.body.appendChild(toast);
+            return toast.present();
+
+        }
     }
+
 }
 
 customElements.define('login-page', LoginPage);
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,9 +1,64 @@
+const createAndInjectionMenu = () => {
+    if (document.querySelector('ion-menu'))
+        return
+
+    const mainContent = document.querySelector('ion-nav');
+    const contentId = 'main-content';
+
+    mainContent.id = contentId;
+
+    const menu = document.createElement('ion-menu');
+    menu.contentId = contentId;
+
+    menu.innerHTML = `
+        <ion-header>
+            <ion-toolbar color='secundary'>
+                <ion-title>Menu</ion-title>         
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content>
+            <ion-list>
+                <ion-item button class="menu-item" data-url="/home">
+                    <ion-label>Home</ion-label>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    `;
+
+    menu.querySelectorAll('.menu-item').forEach(item => {
+        item.addEventListener('click', async () => {
+            const url = item.dataset.url;
+            const router = document.querySelector('ion-router');
+            
+            //verificando se a rota existe
+            if (router && window.location.hash.substring(1) !== url)
+                router.push(url, 'root')
+
+            await menu.close();
+        })
+
+    });
+
+     document.body.prepend(menu);
+
+}
+
 export function createHeader(pageName) {
+    //validar de se nao e pagina de login
+    if (pageName !== 'login')
+        createAndInjectionMenu();
+
+    const start = pageName !== 'login' ? `<ion-button>
+        <ion-menu-button slot='start'></ion-menu-button>
+    </ion-button>`:
+    '<ion-icon name="cafe" style="margin-left: 15px; font-size: 24px;" slot="start"></ion-icon>'
+    
     return `
         <ion-header>
-          <ion-toolbar style="--background: #5d4037; --color: white;">
-                <ion-icon name="cafe" style="margin-left: 15px; font-size: 24px;" slot="start"></ion-icon>
-                <ion-title>Quero Café Bar - ${pageName}</ion-title>
+          <ion-toolbar style="--background: primary; --color: white;">
+                ${start}
+               <ion-title>Quero Café Bar - ${pageName}</ion-title>
             </ion-toolbar>
         </ion-header>
     `;
